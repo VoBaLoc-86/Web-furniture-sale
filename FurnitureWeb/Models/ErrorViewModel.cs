f7c407ffe58ec31227e1e6110e55eea60885729c
vobaloc-86 <vobaloc862005@gmail.com>
2024-11-24 19:10:37 +0700

    Chuyển theme sang .NET

---
+++ FurnitureWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FurnitureWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
